d809f444f27ab8349037140358ab2dc0e16e602b
June <545329942@qq.com>
2016-10-04 14:03:54 +0800

    list thumbs

--- ScanMediaVideo/app/src/main/java/cn/cj/mediascanner/VideoListActivity.java
+++ ScanMediaVideo/app/src/main/java/cn/cj/mediascanner/VideoListActivity.java
@@ -7,20 +7,26 @@ import android.support.v4.app.LoaderManager;
 import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import android.widget.GridView;
 
 /**
  * LoaderManager, CursorLoader, 视频文件浏览
  */
 public class VideoListActivity extends AppCompatActivity {
 
+    GridView gridView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video_list);
 
+        gridView = (GridView) findViewById(R.id.grid_view);
+
         m();
     }
+
+
 
     private void m() {
         // MediaStore.Video.Media.DATA：视频文件路径；
@@ -51,21 +57,23 @@ public class VideoListActivity extends AppCompatActivity {
         }
 
         @Override
-        public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
-            int count = 1;
-            for (data.moveToFirst(); !data.isAfterLast(); data.moveToNext()) {
-                String _title = data.getString(data.getColumnIndex(MediaStore.Video.VideoColumns.TITLE));
-                String _display_name = data.getString(data.getColumnIndex(MediaStore.Video.VideoColumns.DISPLAY_NAME));
-                String _data = data.getString(data.getColumnIndex(MediaStore.Video.VideoColumns.DATA));
-                String _mime_type = data.getString(data.getColumnIndex(MediaStore.Video.VideoColumns.MIME_TYPE));
+        public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+//            int count = 1;
+//            for (cursor.moveToFirst(); !cursor.isAfterLast(); cursor.moveToNext()) {
+//                String _title = cursor.getString(cursor.getColumnIndex(MediaStore.Video.VideoColumns.TITLE));
+//                String _display_name = cursor.getString(cursor.getColumnIndex(MediaStore.Video.VideoColumns.DISPLAY_NAME));
+//                String _data = cursor.getString(cursor.getColumnIndex(MediaStore.Video.VideoColumns.DATA));
+//                String _mime_type = cursor.getString(cursor.getColumnIndex(MediaStore.Video.VideoColumns.MIME_TYPE));
+//
+//                Log.d("---", (count++) + "---------------------------------------------------");
+//                Log.d("---", "title:" + _title + "\n");
+//                Log.d("---", "display_name:" + _display_name + "\n");
+//                Log.d("---", "data:" + _data + "\n");
+//                Log.d("---", "mime_type:" + _mime_type + "\n");
+//            }
 
-                Log.d("---", (count++) + "---------------------------------------------------");
-                Log.d("---", "title:" + _title + "\n");
-                Log.d("---", "display_name:" + _display_name + "\n");
-                Log.d("---", "data:" + _data + "\n");
-                Log.d("---", "mime_type:" + _mime_type + "\n");
-            }
-
+            MyAdapter adapter = new MyAdapter(getApplicationContext(), cursor, false);
+            gridView.setAdapter(adapter);
         }
 
         @Override
